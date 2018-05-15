0f1dcea990121a58fa1140c9d2f50f9ada650a7d
lschmittling <34427873+lschmittling@users.noreply.github.com>
2018-05-15 14:53:37 -0500

    JAWSDB more changes

--- server.js
+++ server.js
@@ -1,13 +1,12 @@
 var express = require("express");
-
+var models = model("./models");
 var bodyParser = require("body-parser");
 var passport = require("passport");
 var session = require("express-session");
 var env = require("dotenv").load();
 
-var PORT = process.env.PORT || 8080;
-
 var app = express();
+var PORT = process.env.PORT || 8080;
 
 app.use(express.static("public"));
 
@@ -22,9 +21,6 @@ app.use(passport.session()); // persistent login sessions
 
 // Set Handlebars.
 var exphbs = require("express-handlebars");
-
-// Models
-var models = require("./models");
 
 // Authorization Route
 var authRoute = require('./routes/auth.js')(app, passport);
@@ -41,10 +37,10 @@ var routes = require("./controllers/authcontroller.js");
 // app.use(routes);
 
 // For testing
-app.get('/', function(req, res) {
-  res.render('index');
+//app.get('/', function(req, res) {
+  //res.render('index');
   // res.send('Welcome to Passport with Sequelize');
-});
+//});
 
 // Load api-routes
 require('./routes/dish-review-routes.js')(app);
