c91dfd66a998fd7208e13c0b4103ed833ba831d8
Zack Muenz <zackmuenz@gmail.com>
2018-05-14 21:10:55 -0500

    ReAdded User.js model

---
+++ models/user.js
@@ -0,0 +1,21 @@
+module.exports = function (sequelize, Sequelize) {
+    var User = sequelize.define('user', {
+        id: { autoIncrement: true, primaryKey: true, type: Sequelize.INTEGER },
+        username: { type: Sequelize.TEXT },
+        password: { type: Sequelize.STRING, allowNull: false },
+        status: {
+            type: Sequelize.ENUM('active', 'inactive'),
+            defaultValue: 'active'
+        }
+    });
+
+    User.associate = function (models) {
+        // Associating User with Restaurants
+        // When a User is deleted, also delete any associated Restaurants
+        User.hasMany(models.DishReview, {
+            onDelete: "cascade"
+        });
+    };
+
+    return User;
+};
